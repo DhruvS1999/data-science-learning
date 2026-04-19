96d70faee681b9ade301ed148b64bc42b49d1b65
DhruvS1999 <dhruvsingh609@gmail.com>
2026-04-19 17:50:31 +0530

    Add files via upload

---
+++ pyhton_bacis_day1.py
@@ -0,0 +1,79 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "747c2261-5b25-4517-9fdc-07c416e52265",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Dhruv\n",
+      "26\n",
+      "15\n",
+      "50\n",
+      "[1, 2, 3, 4, 5]\n",
+      "1\n",
+      "2\n",
+      "3\n",
+      "4\n",
+      "5\n"
+     ]
+    }
+   ],
+   "source": [
+    "#variables\n",
+    "name = \"Dhruv\"\n",
+    "age = \"26\"\n",
+    "\n",
+    "print(name)\n",
+    "print(age)\n",
+    "\n",
+    "#numbers\n",
+    "a = 10\n",
+    "b = 5\n",
+    "print(a+b)\n",
+    "print(a*b)\n",
+    "\n",
+    "#list\n",
+    "numbers = [1,2,3,4,5]\n",
+    "print(numbers)\n",
+    "\n",
+    "#loop\n",
+    "for num in numbers:\n",
+    "    print(num)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9bfd5d36-2ea6-4fa8-9320-028a70821582",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
